c47eb5fc745f3dcbdcc71dbc760f26f8bd5b277f
KFH <KFH@LAPTOP-83314>
2024-05-22 09:44:47 +0300

    added admin controllre

---
+++ KFHRBackEnd/Models/Entites/AddEmployees.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KFHRBackEnd.Models.Entites
+{
+    public class AddEmployees
+    {
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public Role Role { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public DateTime DOB { get; set; }
+
+        [Required]
+        public Gender Gender { get; set; }
+
+        [Required]
+        [Url]
+        public string ProfilePicURL { get; set; }
+
+        [Required]
+        public NFC NFCId { get; set; }
+
+        [Required]
+
+        public Position PositionId { get; set; }
+
+        [Required]
+        public Department DepartmentId { get; set; }
+
+        [Required]
+        public int PointEarned { get; set; }
+    }
+}
