f8b53616c66236ebbc052d491a183baf6ae26f04
Alkousothman@gmail.com <Alkousothman@gmail.com>
2024-05-23 11:20:15 +0300

    Done with leaves page

---
+++ KFHRFrontEnd/Models/EmployeeLeave.cs
@@ -0,0 +1,12 @@
+﻿namespace KFHRFrontEnd.Models
+{
+    public class EmployeeLeave
+    {
+        public string EmployeeName { get; set; }
+        public string EmployeeID { get; set; }
+        public string Position { get; set; }
+        public string DateRange { get; set; }
+        public string Status { get; set; }
+        public string Duration { get; set; }
+    }
+}
