6241bbe5da2ae2948ec4f0ed25109b8ac8be8c9b
firas <firasshdad@gmail.com>
2024-05-25 14:36:34 +0300

    Added AllEmployees page, made some changes.

---
+++ KFHRFrontEnd/Models/AllEmployee.cs
@@ -0,0 +1,12 @@
+﻿namespace KFHRFrontEnd.Models
+{
+    public class AllEmployee
+    {
+        public string EmployeeName { get; set; }
+        public string EmployeeID { get; set; }
+        public string Department { get; set; }
+        public string Position { get; set; }
+        
+      
+    }
+}
