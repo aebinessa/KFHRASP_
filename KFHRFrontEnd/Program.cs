b18a68fefd4fe6d310f03c462bd63bfdc257cc77
firas <firasshdad@gmail.com>
2024-05-26 15:52:20 +0300

    Added icons for adding an employee and an export icon and made some changes.

--- KFHRFrontEnd/Program.cs
+++ KFHRFrontEnd/Program.cs
@@ -1,5 +1,6 @@
 using KFHRFrontEnd.Components;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -20,6 +21,8 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
+
+
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
