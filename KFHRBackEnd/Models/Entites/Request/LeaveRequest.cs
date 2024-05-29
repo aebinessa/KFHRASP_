5ab1b11d57d973cc8b391856f234392db575a4c8
KFH <KFH@LAPTOP-83314>
2024-05-29 10:59:11 +0300

    added leave

---
+++ KFHRBackEnd/Models/Entites/Request/LeaveRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KFHRBackEnd.Models.Entites.Request.Employee
+{
+    public class LeaveRequest
+    {
+       
+        public string LeaveType { get; set; } //enum
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Notes { get; set; }
+    }
+}
