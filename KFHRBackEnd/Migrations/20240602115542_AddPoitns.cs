5852ca772365a657a193597d4bfd8fe51415982e
AHMAD <almusallam296@hotmail.com>
2024-06-02 05:05:30 -0700

    Done

---
+++ KFHRBackEnd/Migrations/20240602115542_AddPoitns.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KFHRBackEnd.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPoitns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RewardPoints",
+                table: "RecommendedCertificates",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RewardPoints",
+                table: "RecommendedCertificates");
+
+        }
+    }
+}
