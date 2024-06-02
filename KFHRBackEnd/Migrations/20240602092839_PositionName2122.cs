eee257f09560737f9219b284df9b139aac22971c
AHMAD <almusallam296@hotmail.com>
2024-06-02 03:21:59 -0700

    d

---
+++ KFHRBackEnd/Migrations/20240602092839_PositionName2122.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KFHRBackEnd.Migrations
+{
+    /// <inheritdoc />
+    public partial class PositionName2122 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RewordPoints",
+                table: "RecommendedCertificates",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RewordPoints",
+                table: "RecommendedCertificates");
+        }
+    }
+}
---
+++ KFHRBackEnd/Models/EmployeeRes.cs
@@ -0,0 +1,30 @@
+﻿using KFHRBackEnd.Models.Entites;
+using System.ComponentModel.DataAnnotations;
+
+public class EmployeeRes
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    [EmailAddress]
+    public string Email { get; set; }
+
+    public DateTime DOB { get; set; }
+
+    public string? Gender { get; set; }
+
+    [Url]
+    public string? ProfilePicURL { get; set; }
+
+    public int? NFCIdNumber { get; set; }
+
+
+    public string? PositionName { get; set; }
+
+    public Department DepartmentName { get; set; }
+
+
+    public int? PointEarned { get; set; }
+
+    public bool IsAdmin { get; set; }
+}
