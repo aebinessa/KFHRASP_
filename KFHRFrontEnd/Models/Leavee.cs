aedf9d17e070da5299f1cd0817b4b7a74e9881da
Alkousothman@gmail.com <Alkousothman@gmail.com>
2024-06-04 10:47:43 +0300

    Working on leaves page

---
+++ KFHRFrontEnd/Models/Leavee.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KFHRFrontEnd.Models
+{
+    public class Leavee
+    {
+        
+            public int ID { get; set; }
+            [Required]
+            public int EmployeeId { get; set; }
+            [Required]
+            public string LeaveType { get; set; }
+            [Required]
+            public DateTime StartDate { get; set; }
+            [Required]
+            public DateTime EndDate { get; set; }
+            [Required]
+            public string Status { get; set; }
+            [Required]
+            public string Notes { get; set; }
+
+
+        
+    }
+}
