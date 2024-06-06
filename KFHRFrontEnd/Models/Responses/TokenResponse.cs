ab105825bc43a3e1c70d401e467d573542b8cc61
firas <firasshdad@gmail.com>
2024-06-06 11:28:58 +0300

    Made some changes.

--- KFHRFrontEnd/Models/Responses/TokenResponse.cs
+++ KFHRFrontEnd/Models/Responses/TokenResponse.cs
@@ -3,5 +3,10 @@
     public class TokenResponse
     {
         public string Token { get; set; }
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeePic { get; set; }
+        public object EmployeeNfc { get; set; }
+        public object EmployeeDepartmentId { get; set; }
     }
 }
