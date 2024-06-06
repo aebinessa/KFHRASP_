293b1687f0115a0342e0fe907f1ab79aeae97b0d
firas <firasshdad@gmail.com>
2024-06-06 12:48:09 +0300

    finished integration.

--- KFHRFrontEnd/Models/EmployeeLeaves.cs
+++ KFHRFrontEnd/Models/EmployeeLeaves.cs
@@ -2,8 +2,10 @@
 {
     public class EmployeeLeaves
     {
+        public int ID { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+        public string Status { get; set; }
 
     }
 }
